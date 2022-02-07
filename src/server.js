cd2151044674c100674c5655186886f71cb20bed
cezarBZ <cezar30santos@gmail.com>
2022-02-07 17:12:39 -0300

    adcionando middleware de cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -8,6 +8,28 @@ const app = express()
 
 // conectando com o mongo
 db.connect()
+
+// habilita cors
+ /* const allowedOrigins= [
+     'http://127.0.0.1:5500',
+     'http://www.app.com.br'
+ ]
+ app.use(cors({
+     origin: function(origin, callback){
+         let allowed = true
+
+         if (!origin) allowed = true
+            
+         if (!allowedOrigins.includes(origin)) allowed = false 
+  
+         callback(null, allowed)
+     }
+ })) */
+
+ app.use(cors({ // DANDO PERMISSÃO APENAS A UMA APLICAÇÃO
+      origin: 'http://127.0.0.1:5500'
+  })) 
+///////////////////////////////////////////////
 
 // habilita server para receber json
 app.use(express.json())
